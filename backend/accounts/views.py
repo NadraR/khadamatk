00cfeec3fdbcd4b07dec581ca773c2826f994e97
NadraR <nadra.rashad@gmail.com>
2025-08-18 15:33:18 +0300

    register-edits

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,18 +2,22 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework import status
 from .models import User, WorkerProfile, ClientProfile
-from .serializers import UserRegisterSerializer, WorkerProfileSerializer, ClientProfileSerializer
-
+from .serializers import CustomUserCreateSerializer, WorkerProfileSerializer, ClientProfileSerializer
 
 class UserRegisterView(generics.CreateAPIView):
-    serializer_class = UserRegisterSerializer
+    serializer_class = CustomUserCreateSerializer
     permission_classes = [permissions.AllowAny]
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        return Response({"message": "User registered successfully", "role": user.role}, status=status.HTTP_201_CREATED)
+        
+        # Automatically create the profile based on the role
+        response_data = serializer.data
+        response_data['message'] = 'User registered successfully'
+        
+        return Response(response_data, status=status.HTTP_201_CREATED)
 
 
 class WorkerProfileCreateView(generics.CreateAPIView):
