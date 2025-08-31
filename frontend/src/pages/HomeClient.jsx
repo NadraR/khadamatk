6ce9a6e8f738b3bdb79e8af494a7dfabffd0570d
rewan24 <rawanazab038@gmail.com>
2025-08-31 23:05:54 +0300

    Home updates

--- frontend/src/pages/HomeClient.jsx
+++ frontend/src/pages/HomeClient.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
+// import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
 import Slider from '../components/Slider';
 import Stats from '../components/Stats';
